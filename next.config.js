7f74ae8939cd8f6da74da0a27a816dd6f96065c9
ShiroiKiku <ividevil@mail.ru>
2022-05-11 17:05:55 +0400

    main

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+};
+module.exports = {
+    serverRuntimeConfig: {
+        secret: "4f1efcda-1644-4759-ac6c-59e510b6d530",
+    },
+    publicRuntimeConfig: {
+        apiUrl:
+            process.env.NODE_ENV === "development"
+                ? "http://localhost:3000/api" // development api
+                : "http://localhost:3000/api", // production api
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
